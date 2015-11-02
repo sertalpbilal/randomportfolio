dec58508c7112e8581e8a7d82d3411eb30e620d3
Sertalp B. Cay <sertalpbilal@gmail.com>
2015-11-02 06:16:38 -0800

    First stable version, documentation is updated

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
-Random Portfolio Dataset Generation
+# Random Portfolio Dataset Generation
 
 This package provides a simplified way to generate random variance-covariance matrix for portfolio optimization problems.
 
 It follows the paper of Hirschberger et al. to generate the random matrix. The rest is shaped for my research needs, but can be modified for any purpose.
+
+## Usage
+
+Inside /src folder, call
+
+```
+randomportfolio.py [-h] -n ASSET -f FILENAME [-s SEED] [-e PARAM_E]
+                        [-v PARAM_V] [-b PARAM_E_BAR]
+
+Arguments:
+  -h, --help            show help message and exit
+  -n ASSET, --asset ASSET
+                        number of assets
+  -f FILENAME, --filename FILENAME
+                        file name
+  -s SEED, --seed SEED  seed number
+  -e PARAM_E, --param_e PARAM_E
+                        the e parameter for the method
+  -v PARAM_V, --param_v PARAM_V
+                        the v parameter for the method
+  -b PARAM_E_BAR, --param_e_bar PARAM_E_BAR
+                        the e-bar parameter for the method
+```
+
+## Example
+
+```
+> python randomportfolio.py -n 200 -f RD1 -s 1122015
+Random Portfolio
+----------------
+Parsing info...
+Generating root...
+Saving to file...
+All done!
+```
+
+Randomly generated data can be found under /data/Q, /data/M, /data/mu, and /data/P
+
+# Reference
+
+Hirschberger, Markus, Yue Qi, and Ralph E. Steuer. "Randomly generating portfolio-selection covariance matrices with specified distributional characteristics." European Journal of Operational Research 177.3 (2007): 1610-1625.
--- src/randomportfolio.py
+++ src/randomportfolio.py
@@ -2,6 +2,7 @@
 """
     randomportfolio
     Random Portfolio: A random covariance generator for portfolio optimization
+    Generates variance-covariance matrix Q, return rates mu, batch size M and price vector P
     :author: Sertalp B. Cay    
 """
 
@@ -13,6 +14,10 @@ import sys
 def saveToFile():
     """ Saves the resulting matrix as a space separated file """    
     print "Saving to file..."
+    numpy.savetxt('../data/Q/' + args.filename, qmatrix, delimiter='\t')
+    numpy.savetxt('../data/M/' + args.filename, mvector, delimiter='\t')
+    numpy.savetxt('../data/mu/' + args.filename, muvector, delimiter='\t')
+    numpy.savetxt('../data/P/' + args.filename, pvector, delimiter='\t')
 
 def eveeMode():
     """ Generates the root vector L by using ev - \bar{e} method """
@@ -20,21 +25,31 @@ def eveeMode():
     # Set seed
     numpy.random.seed(args.seed)
     mu, sigma = 0, 0.1 # mean and standard deviation
-    randnums = numpy.random.normal(mu, sigma, args.asset*args.asset)
     m = max(1, round (( args.param_e_bar * args.param_e_bar - args.param_e * args.param_e) / args.param_v))
     e_hat = math.sqrt(args.param_e / m)
     v_hat = - math.pow(e_hat,2) + math.sqrt( math.pow(e_hat, 4) + (args.param_v / m) )
+    # Generate random numbers
+    randnums = numpy.random.normal(mu, sigma, args.asset*m)
+    f = [ e_hat + math.sqrt(v_hat)*r for r in randnums ]
     #f = e_hat + math.sqrt(v_hat) * randnums
-    print m, e_hat, v_hat
-    # print f
+    f = numpy.mat(f)
+    f = f.reshape(args.asset, m)
+    global qmatrix
+    qmatrix = numpy.dot(f, f.T)
+    global mvector
+    mvector = numpy.random.randint(1, high=100, size=args.asset)
+    global muvector
+    muvector = numpy.random.uniform(-0.1, high=0.1, size=args.asset)
+    global pvector
+    pvector = numpy.random.randint(10, high=125, size=args.asset)
     
-
 def parseInfo():
     """ Takes the user input and parse parameters """
     print "Parsing info..."
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", type=int, help="output verbosity")
     parser.add_argument("-n", "--asset", type=int, help="number of assets", required=True)
+    parser.add_argument("-f", "--filename", help="file name", required=True)
     parser.add_argument("-s", "--seed", type=int,  help="seed number", default=42)
     parser.add_argument("-e", "--param_e", type=float, help="the e parameter for the method", default=0.002)
     parser.add_argument("-v", "--param_v", type=float, help="the v parameter for the method", default=0.000004)
